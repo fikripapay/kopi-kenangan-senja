5afbf49678418867b562fb59ec19c82f434130eb
Abdul Fikri <fikriabdul679@gmail.com>
2024-02-15 18:55:02 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,69 @@
-// Toggle class active
+// Toggle class active humberger
 const navbarNav = document.querySelector(".navbar-nav");
 const humberger = document.querySelector("#humberger-menu");
 
-humberger.addEventListener("click", function () {
+humberger.addEventListener("click", function (e) {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 });
 
-// Klik diluar sidebar untuk hilangkan nav
+// Toggle class active search
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Toggle class active cart
+const btnCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+  btnCart.classList.toggle("active");
+  e.preventDefault();
+};
+
+// Klik diluar element
+const sb = document.querySelector("#search-button");
+const scb = document.querySelector("#shopping-cart-button");
+
 document.addEventListener("click", function (e) {
   if (!humberger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
+  }
+
+  if (!btnCart.contains(e.target) && !scb.contains(e.target)) {
+    btnCart.classList.remove("active");
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButtons = document.querySelectorAll(".item-detail-button");
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+// Klik tombol close
+document.querySelector(".modal .close-icon").onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+// Klik diluar modal
+const modal = document.querySelector("#item-detail-modal");
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
